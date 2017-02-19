6fc31d62bf521a28ff692f695ef83848988745ee
javier aldana <javieraldana@MacBook-Pro-de-javier.local>
2017-02-19 17:43:34 +0100

    url base de datos

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 const config = {
   APP_ID: '1912606492305361',
   APP_SECRET: '34b0316b36ab507674907ced3e1f8b93',
-  DB_URL: 'mongodb://localhost:27017',
+  DB_URL: 'mongodb://yulianross:yulianross@ds157559.mlab.com:57559/facebook-db',
   HUB_VERIFY_TOKEN: '34b0316b36ab507674907ced3e1f8b93'
 };
 
